5ee4f36ead7b9c43ba4258ad1a686ee74a3bc516
nirmaldavis <nirmal.davis@gmail.com>
2016-02-21 13:41:53 +0530

    Custom matcher
    A Custom matcher sample with jasmine unit testing

--- spec/helloworld-spec.js
+++ spec/helloworld-spec.js
@@ -1,5 +1,82 @@
-describe("Hello World", function() {
-    it("should return hello world", function() {
+describe("Hello World", function () {
+    beforeEach(function () {
+
+        //Custom matcher sample
+        
+        jasmine.addMatchers({
+            toBeLargerThan: function () {
+
+                return {
+                    compare: function (actual, expected) {
+
+                        var result = {};
+
+                        result.pass = actual > expected;
+
+                        if (result.pass) {
+                            result.message = actual + " is greater than " + expected;
+                        } else {
+                            result.message = actual + " is not greater than " + expected;
+                        }
+
+                        return result;
+
+                    }
+                };
+            }
+        });
+        
+        //Adding another matcher
+        jasmine.addMatchers(
+        
+            //A custom matcher object
+            {
+                //Matcher function name
+                toContainHello : function() {
+                
+                    //Matcher function should return an object containing 'compare' method
+                    
+                    return { //return object of matcher function
+                        compare : function(actual, expected) {
+                        
+                            //'compare' function should return an object containing 'pass'
+                            
+                            var result = {};
+                            result.pass = true;
+                            
+                            result.pass = (actual.indexOf("Hello") > -1 );
+                            
+                            if(!result.pass) {
+                                result.message = actual + " does not contain a Hello..!!";
+                            }
+                            return result;
+                        }
+                    }; 
+                    
+                } //End of matcher function
+                
+            }//End of custom matcher object
+        );
+        
+    });
+
+    it("should return hello world", function () {
         expect(helloworld()).toEqual("Hello World");
+    });
+
+    it("should contain a hello", function () {
+        expect(helloworld()).toContain("Hello");
+    });
+
+    it("should return hello <user> when passed a <user>", function () {
+        expect(helloworld("Nirmal Davis")).toEqual("Hello Nirmal Davis");
+    });
+
+    it("should match a hello", function () {
+        expect(helloworld()).toContainHello();
+    });
+
+    it("should be a large number", function () {
+        expect(102).toBeLargerThan(5);
     });
 });
--- src/helloworld.js
+++ src/helloworld.js
@@ -1,3 +1,10 @@
-function helloworld() {
+function helloworld(user) {
+    
+    console.log("arguments : " + arguments.length);
+    
+    if(user) {
+        console.log("Hello " + user);
+        return ("Hello " + user);
+    }
     return "Hello World";
 }
